c95fb884407b018bb1af5782f6b21629c0f52657
Peter Serwylo <peter@serwylo.com>
2017-04-06 19:18:18 +1000

    WIP: Add (not yet tested) test of PTV signature generation.
    Each request to the API must be signed by a key blessed on you by
    the PTV organisation.

--- enabler/src/de/schildbach/pte/PtvProvider.java
+++ enabler/src/de/schildbach/pte/PtvProvider.java
@@ -1,8 +1,15 @@
 package de.schildbach.pte;
 
 import de.schildbach.pte.dto.*;
+import okhttp3.HttpUrl;
 
+import javax.annotation.Nonnull;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class PtvProvider implements NetworkProvider {
@@ -55,5 +62,46 @@ public class PtvProvider implements NetworkProvider {
     @Override
     public Point[] getArea() throws IOException {
         return new Point[0];
+    }
+
+    /**
+     * Adapted from the code provided in the .rtf document by PTV:
+     *   https://static.ptv.vic.gov.au/PTV/PTV%20docs/API/1475462320/PTV-Timetable-API-key-and-signature-document.RTF
+     * @param developerId See the linked RTF document above about obtaining a developer ID.
+     * @return A copy of the url, with both "devid=" and "signature=" query parameters appended.
+     * @throws SecurityException If the HmacSHA1 algorithm can't be found, or the provided private key is invalid.
+     */
+    @Nonnull
+    public static HttpUrl appendSignatureToUrl(HttpUrl url, int developerId, String privateKey) throws SecurityException {
+        String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+        final HttpUrl urlWithDevId = url.newBuilder()
+                .addQueryParameter("devid", Integer.toString(developerId))
+                .build();
+
+        byte[] keyBytes = privateKey.getBytes();
+        byte[] uriBytes = urlWithDevId.toString().getBytes();
+        Key signingKey = new SecretKeySpec(keyBytes, HMAC_SHA1_ALGORITHM);
+
+        try {
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(signingKey);
+
+            byte[] signatureBytes = mac.doFinal(uriBytes);
+            StringBuilder signature = new StringBuilder(signatureBytes.length * 2);
+            for (byte signatureByte : signatureBytes) {
+                int intVal = signatureByte & 0xff;
+                if (intVal < 0x10) {
+                    signature.append("0");
+                }
+                signature.append(Integer.toHexString(intVal));
+            }
+
+            return urlWithDevId.newBuilder()
+                    .addQueryParameter("signature", signature.toString())
+                    .build();
+
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new SecurityException("Error attaching signature to PTV request", e);
+        }
     }
 }
--- enabler/test/de/schildbach/pte/live/PtvProviderLiveTest.java
+++ enabler/test/de/schildbach/pte/live/PtvProviderLiveTest.java
@@ -2,9 +2,10 @@ package de.schildbach.pte.live;
 
 import de.schildbach.pte.PtvProvider;
 import de.schildbach.pte.dto.SuggestLocationsResult;
+import okhttp3.HttpUrl;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class PtvProviderLiveTest extends AbstractProviderLiveTest {
     public PtvProviderLiveTest() {
@@ -12,9 +13,19 @@ public class PtvProviderLiveTest extends AbstractProviderLiveTest {
     }
 
     @Test
-    public void suggestLocations() throws Exception {
-        final SuggestLocationsResult result = suggestLocations("Belgrave");
-        assertNotNull(result);
-        print(result);
+    public void appendSignatures() {
+        String privateKey = "";
+
+        HttpUrl urlWithoutQueryParam = HttpUrl.parse("https://test.example.com");
+        assertEquals(
+                "https://test.example.com?devid=135&signature=",
+                PtvProvider.appendSignatureToUrl(urlWithoutQueryParam, 135, privateKey).toString()
+        );
+
+        HttpUrl urlWithQueryParam = HttpUrl.parse("https://test.example.com?queryParam=BLAH");
+        assertEquals(
+                "https://test.example.com?queryParam=BLAH&devid=135&signature=",
+                PtvProvider.appendSignatureToUrl(urlWithQueryParam, 135, privateKey).toString()
+        );
     }
 }
