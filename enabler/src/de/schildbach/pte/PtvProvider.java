4daf2517f43015b97042b3f951b127d09678eeda
Peter Serwylo <peter@serwylo.com>
2017-04-12 19:31:27 +1000

    PTV: Health Check + Suggested Locations API implemented.
    Includes rudimentary tests, but these should be bulked up in future
    commits.

--- enabler/src/de/schildbach/pte/PtvProvider.java
+++ enabler/src/de/schildbach/pte/PtvProvider.java
@@ -1,9 +1,16 @@
 package de.schildbach.pte;
 
 import de.schildbach.pte.dto.*;
+import de.schildbach.pte.util.HttpClient;
 import okhttp3.HttpUrl;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
@@ -12,16 +19,33 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
-public class PtvProvider implements NetworkProvider {
+/**
+ * Implements the custom API provided by Public Transport Victoria (PTV) in Melbourne, Australia.
+ *
+ * There are both <a href="https://www.data.vic.gov.au/data/dataset//ptv-timetable-api">official docs</a>, and some
+ * much more helpful <a href="">unofficial docs</a> provided by Steve Bennett (unaffiliated with PTV).
+ */
+public class PtvProvider extends AbstractNetworkProvider {
 
-    @Override
-    public NetworkId id() {
-        return NetworkId.MELBOURNE;
+    private static final Logger LOG = LoggerFactory.getLogger(PtvProvider.class);
+
+    private static final HttpUrl API_BASE_URL = new HttpUrl.Builder()
+            .scheme("https")
+            .host("timetableapi.ptv.vic.gov.au")
+            .build();
+
+    private String privateKey;
+    private String developerId;
+
+    public PtvProvider(String developerId, String privateKey) {
+        super(NetworkId.MELBOURNE);
+        this.developerId = developerId;
+        this.privateKey = privateKey;
     }
 
     @Override
-    public boolean hasCapabilities(Capability... capabilities) {
-        return false;
+    public boolean hasCapability(Capability capability) {
+        return true;
     }
 
     @Override
@@ -34,9 +58,129 @@ public class PtvProvider implements NetworkProvider {
         return null;
     }
 
+    /**
+     * Queries the /v2/search endpoint to get public transport stops which match the {@param constraint}.
+     * @see <a href="https://stevage.github.io/PTV-API-doc/#header17">API Docs</a>
+     */
     @Override
     public SuggestLocationsResult suggestLocations(CharSequence constraint) throws IOException {
-        return null;
+        assertHealthCheck();
+
+        final HttpUrl url = appendSignatureToUrl(
+                API_BASE_URL.newBuilder()
+                        .addPathSegments("v2/search")
+                        .addPathSegment(constraint.toString())
+                        .build()
+        );
+
+        final CharSequence response = new HttpClient().get(url);
+        try {
+            final JSONArray jsonResponse = new JSONArray(response.toString());
+            List<SuggestedLocation> locations = new ArrayList<>(jsonResponse.length());
+            for (int i = 0; i < jsonResponse.length(); i ++) {
+                final JSONObject object = jsonResponse.getJSONObject(i);
+                final SuggestedLocation location = parseSuggestedLocation(object, i);
+
+                if (location != null) {
+                    locations.add(location);
+                }
+            }
+            return new SuggestLocationsResult(new ResultHeader(id(), ""), locations);
+        } catch (JSONException e) {
+            throw new IOException("Error parsing JSON response", e);
+        }
+    }
+
+    /**
+     * Ideally this would be @Nonnull-able. But for future proofing this provider against future, currently unknown
+     * attribute values we allow for it to return null.
+     */
+    @Nullable
+    private SuggestedLocation parseSuggestedLocation(JSONObject object, int index) throws JSONException {
+        final Location location = locationFromJSON(object);
+        return location != null ? new SuggestedLocation(locationFromJSON(object), index) : null;
+    }
+
+    /**
+     * Ideally this would be @Nonnull-able. But for future proofing this provider against future, currently unknown
+     * attribute values we allow for it to return null.
+     */
+    @Nullable
+    private Location locationFromJSON(JSONObject object) throws JSONException {
+        final JSONObject result = object.getJSONObject("result");
+        switch (object.getString("type")) {
+            case "stop":
+                return parseSuggestedStop(result);
+
+            case "line":
+                LOG.debug("Ignoring type \"line\" in search results.");
+                return null;
+
+            default:
+                // Ignore types so we don't crash if a new type gets introduced. However it may be worth logging so that
+                // there is a slightly higher chance we will identify and correctly handle the  missing type.
+                LOG.info("Unknown location type \"" + object.getString("type") + "\". Expected either \"stop\" or \"line\"");
+                return null;
+        }
+    }
+
+    /**
+     * Parses the JSON response for a "stop" from the /v2/search API.
+     *
+     * It should contain the following:
+     *
+     * {
+     *   "distance": 0.0,
+     *   "suburb": "Belgrave South",
+     *   "transport_type": "bus",
+     *   "route_type": 2,
+     *   "stop_id": 14985,
+     *   "location_name": "140 Colby Dr ",
+     *   "lat": -37.9302826,
+     *   "lon": 145.3589
+     * }
+     *
+     * @see <a href="https://stevage.github.io/PTV-API-doc/#figure10">API docs</a>
+     */
+    private Location parseSuggestedStop(JSONObject object) throws JSONException {
+        return new Location(
+                LocationType.STATION,
+                Integer.toString(object.getInt("stop_id")),
+                object.getInt("lat"),
+                object.getInt("lon"),
+                object.getString("suburb"),
+                object.getString("location_name"),
+                parseProductsFromJson(object.getString("transport_type"))
+        );
+    }
+
+    // "train", "tram", "bus", "vline" or "nightrider"
+    private Set<Product> parseProductsFromJson(String transportType) {
+        final Set<Product> products = new HashSet<>(1);
+        switch(transportType) {
+            case "train":
+                products.add(Product.SUBURBAN_TRAIN);
+                break;
+
+            case "tram":
+                products.add(Product.TRAM);
+                break;
+
+            case "bus":
+                products.add(Product.BUS);
+                break;
+
+            case "vline":
+                products.add(Product.REGIONAL_TRAIN);
+                break;
+
+            case "nightrider":
+                // Doesn't look like there is a particular type of "Night Bus" available, so just stick with "Bus".
+                products.add(Product.BUS);
+                break;
+        }
+
+        return products;
     }
 
     @Override
@@ -67,41 +211,94 @@ public class PtvProvider implements NetworkProvider {
     /**
      * Adapted from the code provided in the .rtf document by PTV:
      *   https://static.ptv.vic.gov.au/PTV/PTV%20docs/API/1475462320/PTV-Timetable-API-key-and-signature-document.RTF
-     * @param developerId See the linked RTF document above about obtaining a developer ID.
      * @return A copy of the url, with both "devid=" and "signature=" query parameters appended.
      * @throws SecurityException If the HmacSHA1 algorithm can't be found, or the provided private key is invalid.
      */
     @Nonnull
-    public static HttpUrl appendSignatureToUrl(HttpUrl url, int developerId, String privateKey) throws SecurityException {
-        String HMAC_SHA1_ALGORITHM = "HmacSHA1";
-        final HttpUrl urlWithDevId = url.newBuilder()
-                .addQueryParameter("devid", Integer.toString(developerId))
+    public HttpUrl appendSignatureToUrl(@Nonnull final HttpUrl uri) throws SignatureException {
+        final HttpUrl uriWithDevId = uri.newBuilder().addQueryParameter("devid", developerId).build();
+
+        final String pathAndQuery = uriWithDevId.encodedPath() + "?" + uriWithDevId.query();
+        final byte[] uriBytes = pathAndQuery.getBytes();
+        final byte[] keyBytes = privateKey.getBytes();
+
+        final String HMAC_SHA1 = "HmacSHA1";
+        final Key signingKey = new SecretKeySpec(keyBytes, HMAC_SHA1);
+
+        Mac mac;
+        try {
+            mac = Mac.getInstance(HMAC_SHA1);
+            mac.init(signingKey);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new SignatureException(e);
+        }
+
+        final byte[] signatureBytes = mac.doFinal(uriBytes);
+
+        final StringBuilder signature = new StringBuilder(signatureBytes.length * 2);
+        for (byte signatureByte : signatureBytes) {
+            int intVal = signatureByte & 0xff;
+            if (intVal < 0x10) {
+                signature.append("0");
+            }
+            signature.append(Integer.toHexString(intVal));
+        }
+
+        return uriWithDevId.newBuilder().addQueryParameter("signature", signature.toString().toUpperCase()).build();
+    }
+
+    public static class SignatureException extends RuntimeException {
+        SignatureException(Throwable cause) {
+            super("Error while calculating signature of PTV request", cause);
+        }
+    }
+
+    /**
+     * Performs a check of the API end point, including whether the client and server clock
+     * are in sync or not.
+     *
+     * The API documentation suggests to make this call before every other network call you make.
+     *
+     * @see <a href="https://stevage.github.io/PTV-API-doc/#header13">API Docs</a>
+     */
+    private void assertHealthCheck() throws RuntimeException, IOException {
+
+        final HttpUrl url = API_BASE_URL.newBuilder()
+                .addPathSegment("v2")
+                .addPathSegment("healthcheck")
+                .addQueryParameter("timestamp", Long.toString(System.currentTimeMillis() / 1000))
                 .build();
 
-        byte[] keyBytes = privateKey.getBytes();
-        byte[] uriBytes = urlWithDevId.toString().getBytes();
-        Key signingKey = new SecretKeySpec(keyBytes, HMAC_SHA1_ALGORITHM);
+        HttpUrl urlWithSig = null;
+        try {
+            urlWithSig = appendSignatureToUrl(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final CharSequence response = new HttpClient().get(urlWithSig);
 
         try {
-            Mac mac = Mac.getInstance("HmacSHA1");
-            mac.init(signingKey);
+            final JSONObject jsonResponse = new JSONObject(response.toString());
+            final boolean securtityTokenOk = jsonResponse.getBoolean("securityTokenOK");
+            final boolean clientClockOk = jsonResponse.getBoolean("clientClockOK");
+            final boolean memcacheOK = jsonResponse.getBoolean("memcacheOK");
+            final boolean databaseOK = jsonResponse.getBoolean("databaseOK");
 
-            byte[] signatureBytes = mac.doFinal(uriBytes);
-            StringBuilder signature = new StringBuilder(signatureBytes.length * 2);
-            for (byte signatureByte : signatureBytes) {
-                int intVal = signatureByte & 0xff;
-                if (intVal < 0x10) {
-                    signature.append("0");
-                }
-                signature.append(Integer.toHexString(intVal));
+            // Don't fall over on clientClock or memcache problems. The clock being wrong does not
+            // prevent data coming back. The memcache server not working will cause the queries to
+            // be slower. However, securityToken and database will prevent the query from working.
+            if (!securtityTokenOk || !databaseOK) {
+                throw new RuntimeException(
+                        "Health check failed: " +
+                                "[securityToken: " + (securtityTokenOk ? "OK" : "Failed") + "] " +
+                                "[clientClock: " + (clientClockOk ? "OK" : "Failed") + "] " +
+                                "[memcacheOK: " + (memcacheOK ? "OK" : "Failed") + "] " +
+                                "[databaseOK: " + (databaseOK ? "OK" : "Failed") + "]");
             }
 
-            return urlWithDevId.newBuilder()
-                    .addQueryParameter("signature", signature.toString())
-                    .build();
-
-        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            throw new SecurityException("Error attaching signature to PTV request", e);
+        } catch (JSONException e) {
+            throw new IOException("Error parsing response: " + response);
         }
     }
 }
--- enabler/test/de/schildbach/pte/live/PtvProviderLiveTest.java
+++ enabler/test/de/schildbach/pte/live/PtvProviderLiveTest.java
@@ -1,31 +1,74 @@
 package de.schildbach.pte.live;
 
+import de.schildbach.pte.NetworkId;
 import de.schildbach.pte.PtvProvider;
-import de.schildbach.pte.dto.SuggestLocationsResult;
+import de.schildbach.pte.dto.*;
 import okhttp3.HttpUrl;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.EnumSet;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class PtvProviderLiveTest extends AbstractProviderLiveTest {
+
+    private final PtvProvider mockPtvProvider = new PtvProvider("135", "abcdefg");
+
     public PtvProviderLiveTest() {
-        super(new PtvProvider());
+        super(new PtvProvider(secretProperty("devid"), secretProperty("apikey")));
     }
 
     @Test
     public void appendSignatures() {
-        String privateKey = "";
-
         HttpUrl urlWithoutQueryParam = HttpUrl.parse("https://test.example.com");
         assertEquals(
-                "https://test.example.com?devid=135&signature=",
-                PtvProvider.appendSignatureToUrl(urlWithoutQueryParam, 135, privateKey).toString()
+                "Should append devid, and then calculate and append signature",
+                "https://test.example.com/?devid=135&signature=b8714bdbb8c309125b6b4e3aab70cf035d211022",
+                mockPtvProvider.appendSignatureToUrl(urlWithoutQueryParam).toString()
         );
 
         HttpUrl urlWithQueryParam = HttpUrl.parse("https://test.example.com?queryParam=BLAH");
         assertEquals(
-                "https://test.example.com?queryParam=BLAH&devid=135&signature=",
-                PtvProvider.appendSignatureToUrl(urlWithQueryParam, 135, privateKey).toString()
+                "Should retain custom query parameters when appending devid and signature",
+                "https://test.example.com/?queryParam=BLAH&devid=135&signature=28331dd78df59534b7be41e675d2840cc974250f",
+                mockPtvProvider.appendSignatureToUrl(urlWithQueryParam).toString()
         );
     }
+
+    @Test
+    public void suggestLocations() throws IOException {
+        SuggestLocationsResult locations = provider.suggestLocations("Flinders Street");
+
+        assertNotNull(locations);
+        assertTrue(locations.getLocations().size() > 0);
+
+        assertNotNull(locations.header);
+        assertEquals(locations.header.network, NetworkId.MELBOURNE);
+        assertEquals(locations.status, SuggestLocationsResult.Status.OK);
+
+        // Flinders Street is the main train station in Melbourne. If we don't find it, something is wrong with this API.
+        boolean foundFlindersStreetStation = false;
+        for (Location location : locations.getLocations()) {
+            assertNotNull(location.products);
+            assertNotNull(location.id);
+
+            // There are two types of services running out of Flinders Street Station: Regional and Suburban trains.
+            // TODO: These two products should still get combined into the same Location (with two Products)
+            if (location.products.contains(Product.SUBURBAN_TRAIN) && location.id.equals("1071")) {
+                System.out.println(location.toString());
+                assertEquals(location.name, "Flinders Street Station");
+                assertEquals(location.type, LocationType.STATION);
+                assertNotNull(location.products);
+                assertTrue(location.products.contains(Product.SUBURBAN_TRAIN));
+
+                foundFlindersStreetStation = true;
+            }
+        }
+
+        assertTrue(foundFlindersStreetStation);
+    }
+
 }
